fe66905d11a13b2f589774e6f7aafe31225964c0
David Polania <davidpolaniaac@users.noreply.github.com>
2019-04-22 10:46:24 -0500

    add get managements

--- src/extensions/Applications/service/management.js
+++ src/extensions/Applications/service/management.js
@@ -1,7 +1,10 @@
-import { createDocumentManagement } from '../documents/managements';
+import { createDocumentManagement, getDocumentManagements } from '../documents/managements';
 
 const services = {
   createDocument: values => createDocumentManagement(values)
+    .then(response => ({ ok: true, ...response }))
+    .catch(error => ({ ok: false, ...error })),
+  getDocuments: () => getDocumentManagements()
     .then(response => ({ ok: true, ...response }))
     .catch(error => ({ ok: false, ...error })),
 };
