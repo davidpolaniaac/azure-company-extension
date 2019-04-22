cd3a151f801f6d3e2a6b735737b9d7842eb850ac
David Polania <david.polaniaac@upb.edu.co>
2019-04-22 10:38:09 -0500

    add funtions for data storage

--- src/extensions/Applications/storage/storage.js
+++ src/extensions/Applications/storage/storage.js
@@ -1,10 +1,29 @@
 import * as SDK from 'azure-devops-extension-sdk';
 
-export async function createDocument(documentName, value) {
+async function getDataManager() {
   await SDK.ready();
   const accessToken = await SDK.getAccessToken();
   const service = await SDK.getService('ms.vss-features.extension-data-service');
-  const dataManager = await service.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
-  return dataManager.createDocument(documentName, value);
+  return service.getExtensionDataManager(SDK.getExtensionContext().id, accessToken);
+}
+
+export async function createDocument(documentName, value) {
+  const dataManager = await getDataManager();
+  return dataManager.setDocument(documentName, value);
+}
+
+export async function getDocuments(documentName) {
+  const dataManager = await getDataManager();
+  return dataManager.getDocuments(documentName);
+}
+
+export async function deleteDocument(documentName, value) {
+  const dataManager = await getDataManager();
+  return dataManager.deleteDocument(documentName, value.id);
+}
+
+export async function updateDocument(documentName, value) {
+  const dataManager = await getDataManager();
+  return dataManager.updateDocument(documentName, value);
 }
 
