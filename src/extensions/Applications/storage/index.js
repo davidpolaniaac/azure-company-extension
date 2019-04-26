245682f58671fd27ef325ea25c4284351dd0a625
David Polania <david.polaniaac@upb.edu.co>
2019-04-25 19:55:31 -0500

    modified id of element

--- src/extensions/Applications/storage/index.js
+++ src/extensions/Applications/storage/index.js
@@ -1,28 +1,27 @@
-import { getDataManager, normalizeData, createKeyHash } from './utlis';
+import { getDataManager, normalizeData, getDocumentByName } from './utlis';
 import { GENERIC_FIELDS } from '../constants/fields';
 
-export async function createDocument(documentName, value) {
+export async function createDocument(collectionName, value) {
   const dataManager = await getDataManager();
-  const data = await normalizeData(value);
-  return dataManager.createDocument(documentName, data);
+  let data = await getDocumentByName(collectionName, value[GENERIC_FIELDS.NAME]);
+  data = typeof (data) === 'undefined' ? await normalizeData(value) : undefined;
+  return dataManager.createDocument(collectionName, data);
 }
 
-export async function getDocuments(documentName) {
+export async function getDocuments(collectionName) {
   const dataManager = await getDataManager();
-  return dataManager.getDocuments(documentName);
+  return dataManager.getDocuments(collectionName);
 }
 
-export async function deleteDocument(documentName, value) {
+export async function deleteDocument(collectionName, value) {
   const dataManager = await getDataManager();
-  const id = createKeyHash(value);
-  return dataManager.deleteDocument(documentName, id);
+  const data = await getDocumentByName(collectionName, value[GENERIC_FIELDS.NAME]);
+  return dataManager.deleteDocument(collectionName, data.id);
 }
 
-export async function updateDocument(documentName, value) {
+export async function updateDocument(collectionName, value) {
   const dataManager = await getDataManager();
-  const newValue = { name: value[GENERIC_FIELDS.NEW_NAME] };
-  const data = await normalizeData(newValue);
-  const oldId = createKeyHash({ name: value[GENERIC_FIELDS.NAME] });
-  await dataManager.deleteDocument(documentName, oldId);
-  return dataManager.createDocument(documentName, data);
+  const data = await getDocumentByName(collectionName, value[GENERIC_FIELDS.NAME]);
+  data[GENERIC_FIELDS.NAME] = value[GENERIC_FIELDS.NEW_NAME];
+  return dataManager.updateDocument(collectionName, data);
 }
